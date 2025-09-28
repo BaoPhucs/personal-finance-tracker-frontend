26e16cd705d27b6d1f4e8c75af295a366601414a
BaoPhucs <nguyenbaophuc4859@gmail.com>
2025-09-28 16:32:08 +0700

    update base url

--- src/api/axiosClient.js
+++ src/api/axiosClient.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const axiosClient = axios.create({
-    baseURL: import.meta.env.VITE_API_BASE_URL || "http://localhost:5237/api",
+    baseURL: import.meta.env.VITE_API_BASE_URL,
 });
 
 // Tự động gắn token vào header
